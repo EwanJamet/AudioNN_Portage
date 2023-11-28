512cbca5562220e520cc28b9982827c124f98317
Cactus <151628368+EwanJamet@users.noreply.github.com>
2023-11-28 11:53:44 +0100

    Delete MobileNetTens directory

--- MobileNetTens/onnx_test.py
+++
@@ -1,5 +0,0 @@
-import onnx
-
-model_path = "/home/ewan/Desktop/audioset_tagging_cnn/MobileNetTens/MobileNetTens_W.onnx"
-onnx_model = onnx.load(model_path)
-print(onnx.checker.check_model(onnx_model))
